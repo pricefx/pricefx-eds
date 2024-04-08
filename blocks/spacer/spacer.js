01cd37e4cc0c1c606ea76d0d44c65ba8bcfcbac2
Swathi <swathi.raju@bounteous.com>
2024-04-08 15:17:47 +0530

    feat: spacer css changes

--- blocks/spacer/spacer.js
+++ blocks/spacer/spacer.js
@@ -1,14 +1,7 @@
 export default async function decorate(block) {
     const [spacerValue] = block.children;
     block.textContent = '';
-  
-    const spacerEl = document.createElement('div');
-    spacerEl.textContent = block.textContent;
-  
-    const authorEl = document.createElement('div');
-    authorEl.classList.add(spacerValue);
-  
-    block.appendChild(spacerEl);
-    block.appendChild(authorEl);
+    block.classList.add(spacerValue.textContent.trim());
+    block.appendChild(block.textContent);
   }
   
