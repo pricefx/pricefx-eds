5cab9073730df1c68f23f3243b7485612d41f39f
Swathi <swathi.raju@bounteous.com>
2024-04-08 16:49:05 +0530

    feat: spacer lint issue fixes

--- blocks/spacer/spacer.js
+++ blocks/spacer/spacer.js
@@ -1,7 +1,6 @@
 export default async function decorate(block) {
-    const [spacerValue] = block.children;
-    block.textContent = '';
-    block.classList.add(spacerValue.textContent.trim());
-    block.appendChild(block.textContent);
-  }
-  
+  const [spacerValue] = block.children;
+  block.textContent = '';
+  block.classList.add(spacerValue.textContent.trim());
+  block.appendChild(block.textContent);
+}
