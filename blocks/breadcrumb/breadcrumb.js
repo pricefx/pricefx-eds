7507bee560cd4674baa94541bc47566539a5605f
Swathi <swathi.raju@bounteous.com>
2024-04-10 08:13:57 +0530

    feat: breadcrumb js changes

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -32,7 +32,13 @@ const getAllPathsExceptCurrent = async (paths) => {
 const createLink = (path) => {
   const pathLink = document.createElement('a');
   pathLink.href = path.url;
-  pathLink.innerText = path.name;
+  if (path.name !== 'Home') {
+    pathLink.innerText = path.name;
+  } else {
+    pathLink.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+    <path d="M3 12.5L5 10.5M5 10.5L12 3.5L19 10.5M5 10.5V20.5C5 21.0523 5.44772 21.5 6 21.5H9M19 10.5L21 12.5M19 10.5V20.5C19 21.0523 18.5523 21.5 18 21.5H15M9 21.5C9.55228 21.5 10 21.0523 10 20.5V16.5C10 15.9477 10.4477 15.5 11 15.5H13C13.5523 15.5 14 15.9477 14 16.5V20.5C14 21.0523 14.4477 21.5 15 21.5M9 21.5H15" stroke="#484D56" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>`;
+  }
   return pathLink;
 };
 
@@ -53,7 +59,10 @@ export default async function decorate(block) {
     currentPath.innerText = document.querySelector('title').innerText;
     breadcrumbLinks.push(currentPath.outerHTML);
 
-    breadcrumb.innerHTML = breadcrumbLinks.join('<span class="breadcrumb-separator">/</span>');
+    breadcrumb.innerHTML =
+      breadcrumbLinks.join(`<span class="breadcrumb-separator"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+    <path d="M14 5.5L21 12.5M21 12.5L14 19.5M21 12.5L3 12.5" stroke="#484D56" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg></span>`);
     block.append(breadcrumb);
   }, 1000);
 }
