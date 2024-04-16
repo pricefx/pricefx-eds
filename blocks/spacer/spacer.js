8aa2c807a3655eeac7a4f90120e7236e01282185
Swathi <swathi.raju@bounteous.com>
2024-04-16 11:16:14 +0530

    feat: spacer component styles

--- blocks/spacer/spacer.js
+++
@@ -1,6 +0,0 @@
-export default async function decorate(block) {
-  const [spacerData] = block.children;
-  block.textContent = '';
-  block.classList.add(spacerData.textContent.trim());
-  block.appendChild(block.textContent);
-}
