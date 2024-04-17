ddb7f3023b98833af19ff8e388e6b5155673eeae
Swathi <swathi.raju@bounteous.com>
2024-04-17 20:48:23 +0530

    feat: hero banner changes

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,3 +1,20 @@
 export default function decorate(block) {
   console.log(block);
+  const pictureImg = block.children[1]?.querySelector('picture');
+  const elementText = block.children[2]?.querySelector('p')?.textContent.trim();
+  block.innerHTML = '';
+  const rightBox = document.createElement('div');
+  rightBox.classList.add('hero-image-container');
+  rightBox.append(pictureImg);
+  block.append(rightBox);
+
+  const leftBox = document.createElement('div');
+  leftBox.classList.add('hero-content-container');
+  leftBox.append(elementText);
+
+  // addDropdown(leftBox);
+
+  block.prepend(leftBox);
+  // elementContainer.parentElement.remove();
+  // removeEmptyPTags(block);
 }
