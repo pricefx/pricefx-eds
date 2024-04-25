1ccc72ffefcdf8a66164442f744485f99b7b2138
archanaganesan11 <archana.ganesan@bounteous.com>
2024-04-25 19:19:05 +0530

    feat: accordion changes completed

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,4 +1,8 @@
 function hasWrapper(el) {
+  document.addEventListener('DOMContentLoaded', () => {
+    const icon = document.getElementById('plus-icon');
+    icon.setAttribute('aria-expanded', 'false');
+  });
   return !!el.firstElementChild && window.getComputedStyle(el.firstElementChild).display === 'block';
 }
 
@@ -10,7 +14,12 @@ export default function decorate(block) {
     summary.className = 'accordion-item-label';
     summary.append(...label.childNodes);
     if (!hasWrapper(summary)) {
-      summary.innerHTML = `<h3>${summary.innerHTML}</h3>`;
+      summary.innerHTML = `
+        <p>${summary.innerHTML}</p>
+        <button class="accordion-button" aria-expanded="false">
+          <span class="plus-icon" aria-expanded="false"></span>
+        </button>
+     `;
     }
     // decorate accordion item body
     const body = row.children[1];
@@ -25,5 +34,18 @@ export default function decorate(block) {
     row.classList.add('accordion-item-container');
     row.innerHTML = '';
     row.append(details);
+  });
+
+  const plusIcon = document.querySelector('.plus-icon');
+  plusIcon.addEventListener('click', () => {
+    const ariaExpandedState = plusIcon.getAttribute('aria-expanded');
+    const details = document.querySelector('details');
+    if (ariaExpandedState === 'false') {
+      details.setAttribute('open', 'true');
+    } else if (ariaExpandedState === 'true') {
+      details.removeAttribute('open');
+    }
+    const newAriaExpandedState = ariaExpandedState === 'false' ? 'true' : 'false';
+    plusIcon.setAttribute('aria-expanded', newAriaExpandedState);
   });
 }
