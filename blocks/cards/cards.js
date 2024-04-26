6a78dbbffc58d8a7bb0fb8ef8ab8dea5a7376b16
Vaitheeswaran Nagalingam <vaitheeswaran.nagalingam@bounteous.com>
2024-04-26 13:21:19 +0530

    feat: pai 51 updating js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -31,9 +31,9 @@ function generateCardDom(props) {
         <a class="cards-card-link" href="${link ? link.href : '#'}" target="${ctaTarget.textContent.trim() === 'true' ? '_blank' : ''}">
           <div class='cards-card-image'>${picture ? picture.outerHTML : ''}</div>
           <div class='cards-card-body'>
-              ${eyebrow.textContent.trim() !== '' ? `<div class='cards-card-eyebrow'>${eyebrow.textContent.trim().toUpperCase()}</div>` : ``}
-              ${title.children.length > 0 ? `<div class='cards-card-title'><h6>${title.textContent.trim()}</h6></div>` : ``}
-              ${description.children.length > 0 !== '' ? `<div class='cards-card-description'>${description.innerHTML}</div>` : ``}
+              ${eyebrow?.textContent.trim() !== '' ? `<div class='cards-card-eyebrow'>${eyebrow.textContent.trim().toUpperCase()}</div>` : ``}
+              ${title?.children.length > 0 ? `<div class='cards-card-title'><h6>${title.textContent.trim()}</h6></div>` : ``}
+              ${description?.children.length > 0 !== '' ? `<div class='cards-card-description'>${description.innerHTML}</div>` : ``}
               <div class='cards-card-cta'>${decorateCTA(cta, ctaLabel, ctaTarget, isClickable).outerHTML}</div>
           </div>
         </a>
@@ -45,9 +45,9 @@ function generateCardDom(props) {
       <li>
         <div class='cards-card-image'>${picture ? picture.outerHTML : ''}</div>
         <div class='cards-card-body'>
-            ${eyebrow.textContent.trim() !== '' ? `<div class='cards-card-eyebrow'>${eyebrow.textContent.trim().toUpperCase()}</div>` : ``}
-            ${title.children.length > 0 ? `<div class='cards-card-title'><h6>${title.textContent.trim()}</h6></div>` : ``}
-            ${description.children.length > 0 !== '' ? `<div class='cards-card-description'>${description.innerHTML}</div>` : ``}
+            ${eyebrow?.textContent.trim() !== '' ? `<div class='cards-card-eyebrow'>${eyebrow.textContent.trim().toUpperCase()}</div>` : ``}
+            ${title?.children.length > 0 ? `<div class='cards-card-title'><h6>${title.textContent.trim()}</h6></div>` : ``}
+            ${description?.children.length > 0 !== '' ? `<div class='cards-card-description'>${description.innerHTML}</div>` : ``}
             <div class='cards-card-cta'>${decorateCTA(cta, ctaLabel, ctaTarget).outerHTML}</div>
         </div>
       </li>
