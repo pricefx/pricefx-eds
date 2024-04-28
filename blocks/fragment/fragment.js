a2f743023a9216f54e150ef960cd456610ad6834
amogh-daryapurkar <136490218+amogh-daryapurkar@users.noreply.github.com>
2024-04-28 09:05:29 -0400

    feat: Update fragment.js

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -38,8 +38,8 @@ export async function loadFragment(path) {
 }
 
 export default async function decorate(block) {
-  const link = block.querySelector('a');
-  const path = link ? link.getAttribute('href') : block.textContent.trim();
+  const [fragmentPath] = block.children;
+  const path = fragmentPath.textContent.trim();
   const fragment = await loadFragment(path);
   if (fragment) {
     const fragmentSection = fragment.querySelector(':scope .section');
