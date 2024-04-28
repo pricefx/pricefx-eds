2b6d524f6dd96df9542897acaea47ea2e8ceb4ac
Swathi <swathi.raju@bounteous.com>
2024-04-28 09:38:49 +0530

    feat: hero changes

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -58,10 +58,6 @@ export default async function decorate(block) {
       heroRightContainer.append(row.firstElementChild);
       heroRightContainer.classList.add('hero-right-container');
     } else if (index === 6) {
-      /* Height Height Variation */
-
-      heroLeftContainer.classList.add(row.firstElementChild.textContent || 'hero-image-right');
-    } else if (index === 7) {
       /* Pre Header Text */
       if (row.firstElementChild?.textContent !== '') {
         const heroPreHeader = document.createElement('span');
@@ -69,7 +65,7 @@ export default async function decorate(block) {
         heroPreHeader.append(row.firstElementChild);
         heroLeftContainerInner.append(heroPreHeader);
       }
-    } else if (index === 8) {
+    } else if (index === 7) {
       /* Hero Content */
       row.firstElementChild?.classList.add('hero-content-container');
       heroLeftContainerInner.append(row.firstElementChild || '');
