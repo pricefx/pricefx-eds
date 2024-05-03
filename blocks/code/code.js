352714516d1cfa66309723a9e5470cda076fb028
Nandhiniraghu <nandhini.raghu@bounteous.com>
2024-05-03 11:07:07 +0530

    feat: section-style-change
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    Revert "feat: section-style"
    
    This reverts commit 06a16fe97431d81b8f39ae917e6473da802a74f1.
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style
    
    feat: section-style

--- blocks/code/code.js
+++ blocks/code/code.js
@@ -6,4 +6,14 @@ export default async function decorate(block) {
     titleElement.innerHTML = titleData;
     block.append(titleElement);
   }
+  const iframes = block.querySelectorAll('iframe');
+  iframes.forEach((iframe) => {
+    const nextSibling = iframe.nextElementSibling;
+    if (nextSibling && nextSibling.tagName.toLowerCase() === 'a') {
+      nextSibling.style.display = 'block';
+    }
+  });
+  if (iframes.length !== 0) {
+    block.parentElement.classList.add('iframe-width');
+  }
 }
