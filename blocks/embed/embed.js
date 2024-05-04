7d6d9c25f156f0907476a26f52fe6de14e6e03ed
Swathi <swathi.raju@bounteous.com>
2024-05-04 06:51:21 +0530

    feat: hero video integration

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -149,7 +149,7 @@ const loadEmbed = (block, link, autoplay, isPopup) => {
 
 export default function decorate(block) {
   const placeholder = block.querySelector('picture');
-  const link = block.querySelector('a').href;
+  const link = block.querySelector('a')?.href;
   const overlayText = block.children[2]?.textContent.trim();
   const isPopup = block.children[3]?.textContent.trim();
   block.textContent = '';
