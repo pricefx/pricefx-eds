b2095d918b915b03f4da8a16930c40d3b7617e24
Nandhiniraghu <nandhini.raghu@bounteous.com>
2024-05-06 16:21:49 +0530

    feat: review-change

--- blocks/code/code.js
+++ blocks/code/code.js
@@ -7,12 +7,6 @@ export default async function decorate(block) {
     block.append(titleElement);
   }
   const iframes = block.querySelectorAll('iframe');
-  iframes.forEach((iframe) => {
-    const nextSibling = iframe.nextElementSibling;
-    if (nextSibling && nextSibling.tagName.toLowerCase() === 'a') {
-      nextSibling.style.display = 'block';
-    }
-  });
   if (iframes.length !== 0) {
     block.parentElement.classList.add('iframe-width');
   }
