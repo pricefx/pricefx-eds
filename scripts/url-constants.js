13ac83e2e537728627a1c6c66530e0694f2cf017
Muneeskumar Rajendran <muneeskumar.rajendran@bounteous.com>
2024-05-08 22:27:49 +0530

    feat: updated url constant

--- scripts/url-constants.js
+++ scripts/url-constants.js
@@ -2,6 +2,8 @@
 const BASE_CONTENT_PATH = '/content/pricefx/en'; // En base content path
 const BASE_STYLE_GUIDE_PATH = '/content/pricefx/style-guide'; // Style guide base content path
 const PUBLISH_ENDPOINT = 'https://main--pricefx-eds--pricefx.hlx.live';
+const DM_VIDEO_SERVER_URL = 'https://s7d9.scene7.com/is/content/';
+const DM_SERVER_URL = 'https://s7d9.scene7.com/is/image/';
 
 // Export the constants
-export { BASE_CONTENT_PATH, BASE_STYLE_GUIDE_PATH, PUBLISH_ENDPOINT };
+export { BASE_CONTENT_PATH, BASE_STYLE_GUIDE_PATH, PUBLISH_ENDPOINT, DM_VIDEO_SERVER_URL, DM_SERVER_URL };
