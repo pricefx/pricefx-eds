d62329aa34b491a55c3d815fd2add196de310f72
amogh-daryapurkar <136490218+amogh-daryapurkar@users.noreply.github.com>
2024-05-10 06:01:35 -0400

    fix: Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -307,7 +307,7 @@ export default async function decorate(block) {
   const brandLogo = `
     <a class="brand-logo-wrapper" href="/" aria-label="Go to Pricefx homepage"><span class="icon icon-pricefx-logo-light"></span></a>`;
   brandWrapperMobile.innerHTML = brandLogo;
-  decorateIcons(brandWrapperMobile, alt = 'Pricefx Logo');
+  decorateIcon(brandWrapperMobile, alt = 'Pricefx Logo');
   mobileHeader.append(brandWrapperMobile);
 
   const mobileNavControlWrapper = document.createElement('div');
