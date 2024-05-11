4fd3e325c85d3a84fc7ab8614f41c135602af5d8
Swathi <swathi.raju@bounteous.com>
2024-05-11 17:51:55 +0530

    feat: brand page UI

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,4 @@
-// import { environmentMode } from '../../scripts/global-functions.js';
+import { environmentMode } from '../../scripts/global-functions.js';
 
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
@@ -23,29 +23,29 @@ export default function decorate(block) {
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
-        // const picParent = pic.parentElement;
+        const picParent = pic.parentElement;
         if (picWrapper) {
           picWrapper.classList.add('columns-img-col');
         }
 
-        // const linkwrapper = pic.parentElement.nextElementSibling;
-        // const aInsideStrong = linkwrapper?.querySelector('strong a');
-        // const aInsideEM = linkwrapper?.querySelector('em a');
-        // const a = linkwrapper?.querySelector('a');
-        // const isTarget = linkwrapper?.nextElementSibling;
-        // if (a && !aInsideStrong && !aInsideEM) {
-        //   a.textContent = '';
-        //   a.target = isTarget?.textContent.trim() === 'true' ? '_blank' : '';
-        //   a.append(pic);
-        //   picParent.append(a);
+        const linkwrapper = pic.parentElement.nextElementSibling;
+        const aInsideStrong = linkwrapper?.querySelector('strong a');
+        const aInsideEM = linkwrapper?.querySelector('em a');
+        const a = linkwrapper?.querySelector('a');
+        const isTarget = linkwrapper?.nextElementSibling;
+        if (a && !aInsideStrong && !aInsideEM && a.textContent !== 'download') {
+          a.textContent = '';
+          a.target = isTarget?.textContent.trim() === 'true' ? '_blank' : '';
+          a.append(pic);
+          picParent.append(a);
 
-        //   if (environmentMode() === 'publish') {
-        //     linkwrapper.remove();
-        //     if (isTarget) {
-        //       isTarget.remove();
-        //     }
-        //   }
-        // }
+          if (environmentMode() === 'publish') {
+            linkwrapper.remove();
+            if (isTarget) {
+              isTarget.remove();
+            }
+          }
+        }
       }
     });
   });
