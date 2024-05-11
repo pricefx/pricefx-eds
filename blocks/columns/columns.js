1972ca6ce88d53e11d0452dcca4d79e6a7a996a9
Swathi <swathi.raju@bounteous.com>
2024-05-11 14:58:44 +0530

    feat: brand page UI

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,4 @@
-import { environmentMode } from '../../scripts/global-functions.js';
+// import { environmentMode } from '../../scripts/global-functions.js';
 
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
@@ -12,32 +12,39 @@ export default function decorate(block) {
         col.style.background = 'none';
         col.style.boxShadow = 'none';
       }
+
+      if (col.querySelector('a')?.textContent === 'download') {
+        const downloadLink = col.querySelector('a');
+        const downloadImg = `<img src="/content/dam/pricefx/style-guide/download-icon.png" alt="download" />`;
+        downloadLink.innerHTML = downloadImg;
+      }
+
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
-        const picParent = pic.parentElement;
+        // const picParent = pic.parentElement;
         if (picWrapper) {
           picWrapper.classList.add('columns-img-col');
         }
 
-        const linkwrapper = pic.parentElement.nextElementSibling;
-        const aInsideStrong = linkwrapper?.querySelector('strong a');
-        const aInsideEM = linkwrapper?.querySelector('em a');
-        const a = linkwrapper?.querySelector('a');
-        const isTarget = linkwrapper?.nextElementSibling;
-        if (a && !aInsideStrong && !aInsideEM) {
-          a.textContent = '';
-          a.target = isTarget?.textContent.trim() === 'true' ? '_blank' : '';
-          a.append(pic);
-          picParent.append(a);
+        // const linkwrapper = pic.parentElement.nextElementSibling;
+        // const aInsideStrong = linkwrapper?.querySelector('strong a');
+        // const aInsideEM = linkwrapper?.querySelector('em a');
+        // const a = linkwrapper?.querySelector('a');
+        // const isTarget = linkwrapper?.nextElementSibling;
+        // if (a && !aInsideStrong && !aInsideEM) {
+        //   a.textContent = '';
+        //   a.target = isTarget?.textContent.trim() === 'true' ? '_blank' : '';
+        //   a.append(pic);
+        //   picParent.append(a);
 
-          if (environmentMode() === 'publish') {
-            linkwrapper.remove();
-            if (isTarget) {
-              isTarget.remove();
-            }
-          }
-        }
+        //   if (environmentMode() === 'publish') {
+        //     linkwrapper.remove();
+        //     if (isTarget) {
+        //       isTarget.remove();
+        //     }
+        //   }
+        // }
       }
     });
   });
