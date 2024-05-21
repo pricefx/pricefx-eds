b3a1cc507c82c4a6bc5749bc906ead1081fac80c
Swathi <swathi.raju@bounteous.com>
2024-05-21 18:26:24 +0530

    feat: careers update

--- blocks/careers/careers.js
+++ blocks/careers/careers.js
@@ -1,6 +1,6 @@
-async function loadJobsData(block) {
+async function loadJobsData(sortBy, filterBy, block) {
   try {
-    const response = await fetch('https://careers.jobscore.com/jobs/pricefx/feed.json', {
+    const response = await fetch(`https://careers.jobscore.com/jobs/pricefx/feed.json?sort=${filterBy}${sortBy}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -10,6 +10,9 @@ async function loadJobsData(block) {
 
     const { jobs } = jobScore;
     const departments = [];
+    document.querySelector('.careers-postings-container')?.remove();
+    const jobPosting = document.createElement('div');
+    jobPosting.classList.add('careers-postings-container');
 
     jobs.forEach((job) => {
       const departmentName = job.department;
@@ -20,30 +23,32 @@ async function loadJobsData(block) {
         const departmentDiv = document.createElement('div');
         departmentDiv.classList.add('careers-postings');
         departmentDiv.innerHTML = `<h3><strong>${departmentName}</strong></h3>
-      <div class="careers-content-container">
-      <ul id="${departmentID}">
-        <li class="job-item">                        
-          <div class="job-container">                                 
-            <div class="job-header">                                   
-            <h3>${job.title}</h3>   
-            <p>${job.city + job.state} " - " ${job.country} </p>
-            </div>                                   
-            <div class="job-details" data-expanded="false">${job.description}                                     
-              <a href="${job.detail_url}" target="_blank" class="button primary">Apply Now</a>                                 
-              <div class="a2a-default-style">
-                <a class="a2a-button-facebook"></a>
-                <a class="a2a-button-twitter"></a>
-                <a class="a2a-button-pinterest"></a>        
-                <a class="a2a-dd" href="https://www.addtoany.com/share"></a>
+        <div class="careers-content-container">
+        <ul id="${departmentID}">
+          <li class="job-item">                        
+            <div class="job-container">                                 
+              <div class="job-header">                                   
+              <h3>${job.title}</h3>   
+              <p>${job.city + job.state} " - " ${job.country} </p>
+              </div>                                   
+              <div class="job-details" data-expanded="false">${job.description}                                     
+                <a href="${job.detail_url}" target="_blank" class="button primary">Apply Now</a>                                 
+                <div class="a2a-default-style">
+                  <a class="a2a-button-facebook"></a>
+                  <a class="a2a-button-twitter"></a>
+                  <a class="a2a-button-pinterest"></a>        
+                  <a class="a2a-dd" href="https://www.addtoany.com/share"></a>
+                </div>
               </div>
-            </div>
-          </div>                      
-        </li>
-      </ul>
-      </div>`;
-        block.append(departmentDiv);
+            </div>                      
+          </li>
+        </ul>
+        </div>`;
+        jobPosting.append(departmentDiv);
+        block.append(jobPosting);
       }
     });
+
     document.addEventListener('click', (e) => {
       const target = e.target.closest('.job-header');
       if (target) {
@@ -69,7 +74,7 @@ function renderCareers(block) {
   const filterBy = document.createElement('div');
   filterBy.classList.add('careers-filter');
   filterBy.innerHTML = `<select class="form-control" id="filterby" name="filterBy">
-      <option>Sort By</option>
+      <option>- Choose an Option -</option>
       <option value="department" selected>Department</option>
       <option value="title">Title</option>
       <option value="location">Location</option>
@@ -82,6 +87,7 @@ function renderCareers(block) {
   const sortBy = document.createElement('div');
   sortBy.classList.add('careers-sort');
   sortBy.innerHTML = `<select class="form-control" id="sortby" name="sortby">
+    <option>- Choose an Option -</option>
     <option value="" selected>A to Z</option>
     <option value="_reverse">Z to A</option>
   </select>`;
@@ -94,5 +100,18 @@ function renderCareers(block) {
 
 export default async function decorate(block) {
   renderCareers(block);
-  loadJobsData(block);
+  loadJobsData('deparment', '', block);
+  document.addEventListener('change', (e) => {
+    const targetSort = e.target.closest('#sortby');
+    const targetFilter = e.target.closest('#filterby');
+    let sortBy = document.querySelector('#sortby').value;
+    let filterBy = document.querySelector('#filterby').value;
+    if (targetSort) {
+      sortBy = targetSort.value;
+    }
+    if (targetFilter) {
+      filterBy = targetFilter.value;
+    }
+    loadJobsData(sortBy, filterBy, block);
+  });
 }
