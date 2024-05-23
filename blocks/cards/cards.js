dbbb7eb3ac46443d172cf79d0cf6f66d8dfdd4f2
Swathi <swathi.raju@bounteous.com>
2024-05-23 22:32:47 +0530

    feat: im pricing updates in card

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -29,7 +29,7 @@ function generateCardDom(props) {
     const cardDOM = `
           <a class="cards-card-link" href="${link ? link.href : '#'}" target="${ctaTarget.textContent.trim() === 'true' ? '_blank' : ''}">
           <div class='cards-card-image'>${picture ? picture.outerHTML : ''}</div>
-          <div class='cards-card-top-content'>${cardTopContent?.textContent.trim()}</div>
+          <div class='cards-card-top-content'>${cardTopContent.innerHTML}</div>
           <div class='cards-card-body'>
               ${eyebrow?.textContent.trim() !== '' ? `<div class='cards-card-eyebrow'>${eyebrow.textContent.trim().toUpperCase()}</div>` : ``}
               ${title?.children.length > 0 ? `<div class='cards-card-title'><h6>${title.textContent.trim()}</h6></div>` : ``}
@@ -42,7 +42,7 @@ function generateCardDom(props) {
   }
   const cardDOM = `
         <div class='cards-card-image'>${picture ? picture.outerHTML : ''}</div>
-        <div class='cards-card-top-content'>${cardTopContent?.textContent.trim()}</div>
+        <div class='cards-card-top-content'>${cardTopContent.innerHTML}</div>
         <div class='cards-card-body'>
             ${eyebrow?.textContent.trim() !== '' ? `<div class='cards-card-eyebrow'>${eyebrow.textContent.trim().toUpperCase()}</div>` : ``}
             ${title?.children.length > 0 ? `<div class='cards-card-title'><h6>${title.textContent.trim()}</h6></div>` : ``}
