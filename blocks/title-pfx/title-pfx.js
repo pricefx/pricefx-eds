f66f838986b6ba4535c8b47b2a3e25f63c03e13a
Swathi <swathi.raju@bounteous.com>
2024-05-30 12:28:11 +0530

    feat:  title pfx

--- blocks/title-pfx/title-pfx.js
+++ blocks/title-pfx/title-pfx.js
@@ -1,10 +1,10 @@
 export default async function decorate(block) {
-  const [heading, headingType] = block;
+  const [heading, headingType] = block.children;
   const titleText = heading?.textContent.trim() || '';
   const titleType = headingType?.textContent.trim() || 'h2';
   block.textContent = '';
   if (titleText) {
-    const titleElement = block.createElement(titleType);
+    const titleElement = document.createElement(titleType);
     titleElement.innerHTML = titleText;
     block.append(titleElement);
   }
