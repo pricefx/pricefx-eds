f7f34f95f16d78e0b786e051b0d75dd07a0cb2f4
amogh-daryapurkar <136490218+amogh-daryapurkar@users.noreply.github.com>
2024-06-04 21:34:26 -0400

    fix: Update iframe.js

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -7,10 +7,10 @@ const renderBadges = (badges) => {
 
     const badgeLink = document.createElement('a');
     badgeLink.title = badge.textContent;
-    badgeLink.href = "https://www.g2.com/products/pricefx/reviews?utm_source=rewards-badge";
+    badgeLink.href = 'https://www.g2.com/products/pricefx/reviews?utm_source=rewards-badge';
 
     const badgeImg = document.createElement('img');
-    badgeImg.decoding = "async";
+    badgeImg.decoding = 'async';
     badgeImg.alt = badge.textContent;
     badgeImg.dataset.src = badge.href;
     badgeImg.src = badge.href;
@@ -30,17 +30,23 @@ const renderIframes = (iframes, height, width) => {
   if (iframes.length === 3) {
     const leftColumn = document.createElement('div');
     leftColumn.classList.add('iframe__left-column');
-    if (width) leftColumn.style.maxWidth = `${width + 36}px`;
+    if (width) {
+      leftColumn.style.maxWidth = `${width + 36}px`;
+    }
 
     for (let i = 0; i < 2; i++) {
       const iframeContainer = document.createElement('div');
       iframeContainer.classList.add('iframe__container');
-      if (width) iframeContainer.style.maxWidth = `${width + 36}px`;
+      if (width) {
+        iframeContainer.style.maxWidth = `${width + 36}px`;
+      }
 
       const iframe = document.createElement('iframe');
       iframe.src = iframes[i].textContent.trim();
-      iframe.frameBorder = "0";
-      if (height) iframe.style.minHeight = `${height}px`;
+      iframe.frameBorder = '0';
+      if (height) {
+        iframe.style.minHeight = `${height}px`;
+      }
 
       iframeContainer.appendChild(iframe);
       leftColumn.appendChild(iframeContainer);
@@ -48,16 +54,22 @@ const renderIframes = (iframes, height, width) => {
 
     const rightColumn = document.createElement('div');
     rightColumn.classList.add('iframe__right-column');
-    if (width) rightColumn.style.maxWidth = `${width + 36}px`;
+    if (width) {
+      rightColumn.style.maxWidth = `${width + 36}px`;
+    }
 
     const rightIframeContainer = document.createElement('div');
     rightIframeContainer.classList.add('iframe__container');
-    if (width) rightIframeContainer.style.maxWidth = `${width + 36}px`;
+    if (width) {
+      rightIframeContainer.style.maxWidth = `${width + 36}px`;
+    }
 
     const rightIframe = document.createElement('iframe');
     rightIframe.src = iframes[2].textContent.trim();
-    rightIframe.frameBorder = "0";
-    if (height) rightIframe.style.minHeight = `${height + height + 36}px`;
+    rightIframe.frameBorder = '0';
+    if (height) {
+      rightIframe.style.minHeight = `${height + height + 36}px`;
+    }
 
     rightIframeContainer.appendChild(rightIframe);
     rightColumn.appendChild(rightIframeContainer);
@@ -68,12 +80,16 @@ const renderIframes = (iframes, height, width) => {
     iframes.forEach((iframe) => {
       const iframeContainer = document.createElement('div');
       iframeContainer.classList.add('iframe__container');
-      if (width) iframeContainer.style.maxWidth = `${width + 36}px`;
+      if (width) {
+        iframeContainer.style.maxWidth = `${width + 36}px`;
+      }
 
       const iframeElement = document.createElement('iframe');
       iframeElement.src = iframe.textContent.trim();
-      iframeElement.frameBorder = "0";
-      if (height) iframeElement.style.minHeight = `${height}px`;
+      iframeElement.frameBorder = '0';
+      if (height) {
+        iframeElement.style.minHeight = `${height}px`;
+      }
 
       iframeContainer.appendChild(iframeElement);
       fragment.appendChild(iframeContainer);
