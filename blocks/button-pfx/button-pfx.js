9ffa8999a34babfaa656bf476851977575444166
Si-Jia Mao <sijia.mao@bounteous.com>
2024-06-13 18:00:47 -0400

    feat: pai-291 updating button pfx model

--- blocks/button-pfx/button-pfx.js
+++ blocks/button-pfx/button-pfx.js
@@ -1,27 +1,27 @@
 export default async function decorate(block) {
-  const [link, target, buttonLabel, type] = block.children;
-  console.log(block);
-  console.log(block.children);
-  block.textContent = '';
-  const targetValue = target.textContent.trim() === 'true' ? '_blank' : '_self';
+  const [link, target] = block.children;
+  console.log(link);
+  console.log(target);
+  // block.textContent = '';
+  // const targetValue = target.textContent.trim() === 'true' ? '_blank' : '_self';
 
-  if (type.textContent.trim() === 'left-arrow') {
-    block.classList.add('left-arrow');
-  } else if (type.textContent.trim() === 'right-arrow') {
-    block.classList.add('right-arrow');
-  }
+  // if (type.textContent.trim() === 'left-arrow') {
+  //   block.classList.add('left-arrow');
+  // } else if (type.textContent.trim() === 'right-arrow') {
+  //   block.classList.add('right-arrow');
+  // }
 
-  if (link.textContent.trim() === '') {
-    const buttonEl = document.createElement('button');
-    buttonEl.classList.add('button');
-    buttonEl.textContent = buttonLabel.textContent.trim();
-    block.appendChild(buttonEl);
-  } else {
-    const buttonEl = document.createElement('a');
-    buttonEl.classList.add('button');
-    buttonEl.textContent = buttonLabel.textContent.trim();
-    buttonEl.href = link.textContent.trim();
-    buttonEl.setAttribute('target', targetValue);
-    block.appendChild(buttonEl);
-  }
+  // if (link.textContent.trim() === '') {
+  //   const buttonEl = document.createElement('button');
+  //   buttonEl.classList.add('button');
+  //   buttonEl.textContent = buttonLabel.textContent.trim();
+  //   block.appendChild(buttonEl);
+  // } else {
+  //   const buttonEl = document.createElement('a');
+  //   buttonEl.classList.add('button');
+  //   buttonEl.textContent = buttonLabel.textContent.trim();
+  //   buttonEl.href = link.textContent.trim();
+  //   buttonEl.setAttribute('target', targetValue);
+  //   block.appendChild(buttonEl);
+  // }
 }
