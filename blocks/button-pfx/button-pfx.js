aaeee2a69c5962855e6bb55f6620136d72984d6d
Si-Jia Mao <sijia.mao@bounteous.com>
2024-06-13 19:40:54 -0400

    feat: pai-291 updated button pfx js logic to avoid default markup desctruction

--- blocks/button-pfx/button-pfx.js
+++ blocks/button-pfx/button-pfx.js
@@ -1,28 +1,23 @@
 export default async function decorate(block) {
   const [link, target, type] = block.children;
-  console.log(link);
-  console.log(target);
-  console.log(type);
-  // block.textContent = '';
-  // const targetValue = target.textContent.trim() === 'true' ? '_blank' : '_self';
+  const targetValue = target.textContent.trim() === 'true' ? '_blank' : '_self';
 
-  // if (type.textContent.trim() === 'left-arrow') {
-  //   block.classList.add('left-arrow');
-  // } else if (type.textContent.trim() === 'right-arrow') {
-  //   block.classList.add('right-arrow');
-  // }
+  if (type.textContent.trim() === 'left-arrow') {
+    block.classList.add('left-arrow');
+  } else if (type.textContent.trim() === 'right-arrow') {
+    block.classList.add('right-arrow');
+  }
 
-  // if (link.textContent.trim() === '') {
-  //   const buttonEl = document.createElement('button');
-  //   buttonEl.classList.add('button');
-  //   buttonEl.textContent = buttonLabel.textContent.trim();
-  //   block.appendChild(buttonEl);
-  // } else {
-  //   const buttonEl = document.createElement('a');
-  //   buttonEl.classList.add('button');
-  //   buttonEl.textContent = buttonLabel.textContent.trim();
-  //   buttonEl.href = link.textContent.trim();
-  //   buttonEl.setAttribute('target', targetValue);
-  //   block.appendChild(buttonEl);
-  // }
+  if (link.textContent.trim() !== '') {
+    const buttonEl = document.querySelector('a');
+    buttonEl.setAttribute('target', targetValue);
+  }
+
+  if (target.textContent.trim() !== '') {
+    target.classList.add('hidden');
+  }
+
+  if (type.textContent.trim() !== '') {
+    type.classList.add('hidden');
+  }
 }
